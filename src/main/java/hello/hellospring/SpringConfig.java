d24d5dc4ee7eed59880d9010a1f407a3abf7132d
zingwang <zingwang06@naver.com>
2023-01-07 10:17:01 +0900

    h2, jdbc 테스트

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,14 +1,23 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig
 {
+    DataSource dataSource;
+    @Autowired
+    public SpringConfig(DataSource dataSource){
+        this.dataSource=dataSource;
+    }
 
     @Bean
     public MemberService memberService(){
@@ -18,6 +27,8 @@ public class SpringConfig
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
